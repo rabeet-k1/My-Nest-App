b0ebfc5c20fbccc3264f551cbe01ae64fec9e472
rabeet-k1 <rabeet.k1@gmail.com>
2026-09-21 18:01:03 +0500

    changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,15 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppService } from './app.service';
 import { CategoryModule } from './category/category.module';
+import { DatabaseService } from './database/database.service';
 import { EmployeeModule } from './employee/employee.module';
+import { LoggerMiddleware } from './middleware/logger/logger.middleware';
 import { ProductService } from './product/product.service';
 import { StudentModule } from './student/student.module';
-import { LoggerMiddleware } from './middleware/logger/logger.middleware';
 
 @Module({
   imports: [EmployeeModule, CategoryModule, StudentModule],
-  providers: [AppService, ProductService],
+  providers: [AppService, ProductService, DatabaseService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
---
+++ src/database/database.service.ts
@@ -0,0 +1,28 @@
+import {
+  Injectable,
+  OnModuleInit,
+  OnApplicationShutdown,
+} from '@nestjs/common';
+
+@Injectable()
+export class DatabaseService {
+  private isConnected = false;
+
+  onModuleInit() {
+    this.isConnected = true;
+    console.log('Database connected successfully');
+  }
+
+  onApplicationShutdown(signal: string) {
+    this.isConnected = false;
+    console.log(
+      `Database disconnected successfully due to app shutdown. Signal ${signal}`,
+    );
+  }
+
+  getStatus() {
+    return this.isConnected
+      ? 'Database is connected'
+      : 'Database is disconnected';
+  }
+}
